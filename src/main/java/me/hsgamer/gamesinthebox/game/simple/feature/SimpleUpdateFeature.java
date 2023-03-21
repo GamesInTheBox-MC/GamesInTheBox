dd2c3d765cbd65b04eee9a6a065acb4af9ef4c1e
HSGamer <huynhqtienvtag@gmail.com>
2023-03-21 16:44:57 +0700

    schedule to update

--- src/main/java/me/hsgamer/gamesinthebox/game/simple/feature/SimpleUpdateFeature.java
+++ src/main/java/me/hsgamer/gamesinthebox/game/simple/feature/SimpleUpdateFeature.java
@@ -18,28 +18,45 @@ package me.hsgamer.gamesinthebox.game.simple.feature;
 import me.hsgamer.gamesinthebox.game.feature.PointFeature;
 import me.hsgamer.gamesinthebox.game.feature.TopFeature;
 import me.hsgamer.gamesinthebox.game.simple.SimpleGameArena;
+import me.hsgamer.gamesinthebox.planner.feature.PluginFeature;
 import me.hsgamer.minigamecore.base.Feature;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public class SimpleUpdateFeature implements Feature {
+    private final Plugin plugin;
     private final SimpleGameArena arena;
+    private BukkitTask task;
 
     public SimpleUpdateFeature(SimpleGameArena arena) {
+        this.plugin = arena.getPlanner().getFeature(PluginFeature.class).getPlugin();
         this.arena = arena;
     }
 
     public void initState() {
         arena.getFeature(DescriptiveHologramFeature.class).initHologram();
-        arena.getFeature(DescriptiveHologramFeature.class).updateHologram();
+        task = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this::updateState, 0L, 20L);
     }
 
-    public void updateState() {
+    private void updateState() {
         arena.getFeature(PointFeature.class).takeTopSnapshot();
         arena.getFeature(TopFeature.class).setTop(arena.getFeature(PointFeature.class).getTopSnapshotAsStringPair());
         arena.getFeature(DescriptiveHologramFeature.class).updateHologram();
     }
 
     public void clearState() {
+        if (task != null) {
+            task.cancel();
+        }
         arena.getFeature(PointFeature.class).clearPoints();
         arena.getFeature(DescriptiveHologramFeature.class).clearHologram();
+    }
+
+    @Override
+    public void clear() {
+        if (task != null) {
+            task.cancel();
+        }
     }
 }
