b6376ae7f7e043a67e3391cf694a9764a54e7e3b
HSGamer <huynhqtienvtag@gmail.com>
2023-03-23 12:55:33 +0700

    improve comment

--- src/main/java/me/hsgamer/gamesinthebox/planner/feature/package-info.java
+++ src/main/java/me/hsgamer/gamesinthebox/planner/feature/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains the features of the {@link me.hsgamer.gamesinthebox.planner.Planner}
+ * Contains the {@link me.hsgamer.minigamecore.base.Feature} of the {@link me.hsgamer.gamesinthebox.planner.Planner}
  */
 package me.hsgamer.gamesinthebox.planner.feature;
